a26bd493bc00fc54fac6c4afb1b93bcc4aad6c9b
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2020-01-25 15:00:57 -0600

    added the order-page component

--- tamuhack2020/src/app/app.module.ts
+++ tamuhack2020/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrderPageComponent } from './order-page/order-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderPageComponent
   ],
   imports: [
     BrowserModule,
